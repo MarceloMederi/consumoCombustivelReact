96524795b7f78c963b7961b13e12318876dfde95
Marcelo Medeiros dos Santos <marcelo.msantos@sabin.com.br>
2024-03-01 12:48:04 -0300

    Implementado a logica de min e max, e tratamento de de entrada supeior ao max e estilizado a pagina.

--- concom/src/App.jsx
+++ concom/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [kilometerError, setKilometerErro]= useState("");
+  const [averageError,  setAverageError] = useState("");
 
   const validateUser = () => {
     let username = document.getElementById("username").value;
@@ -16,19 +18,44 @@ function App() {
     }
   };
 
+    const handleKilometerChange = (event) => {
+      const value = event.target.value;
+      if (parseInt(value, 10) > 1000) {
+        setKilometerErro("O valor máximo é 1000.");
+      } else {
+        setKilometerErro("");
+      }
+    };
+
+    const handlerAverageChange =(event) => {
+      const value = event.target.value;
+      if (parseInt(value, 10) > 50) {
+        setAverageError("O valor máximo é 50.");
+      } else {
+        setAverageError("");
+      }
+    };
+
   const renderCalculator = () => {
     return (
       <div>
-        <h2>Calculadora</h2>
-        <label htmlFor="km">Informe o km atual do seu carro</label>
-        <input type="num" id="km" required/>
+        <label htmlFor="kilometer">Informe a distância que seu veículo vai percorrer (em km)</label>
+        <input type="number" id="kilometer" min={0} max={1000} required onChange={handleKilometerChange} />
+        <span style={{color: "red"}}>{kilometerError && kilometerError}</span>
+        
+        <label htmlFor="average">Informe a média de consumo do seu veículo (em km/l)</label>
+        <input type="number" id="average" min={0} max={50} required onChange={handlerAverageChange}/>
+        <span style={{color:"red"}}>{averageError && averageError}</span>
+        
+        <label htmlFor="price">Informe o preço do combustível (por litro)</label>
+        <input type="number" id="price" min={0} step={0.01} required/>
       </div>
     );
   };
 
   return (
     <main>
-      <h1>Controle de abastecimento</h1>
+      <h1>Controle de média</h1>
 
       {isAuthenticated ? (
         renderCalculator()
