3f387be15daef5848afdb4b3f7aebc705b6a1ee4
Marcelo Medeiros dos Santos <marcelo.msantos@sabin.com.br>
2024-03-04 12:47:03 -0300

    Alterado o estilo da pagina de login

--- concom/src/App.jsx
+++ concom/src/App.jsx
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [kilometerError, setKilometerErro]= useState("");
-  const [averageError,  setAverageError] = useState("");
+  const [kilometerError, setKilometerErro] = useState("");
+  const [averageError, setAverageError] = useState("");
 
   const validateUser = () => {
     let username = document.getElementById("username").value;
@@ -18,37 +18,43 @@ function App() {
     }
   };
 
-    const handleKilometerChange = (event) => {
-      const value = event.target.value;
-      if (parseInt(value, 10) > 1000) {
-        setKilometerErro("O valor máximo é 1000.");
-      } else {
-        setKilometerErro("");
-      }
-    };
+  const handleKilometerChange = (event) => {
+    const value = event.target.value;
+    if (parseInt(value, 10) > 1000) {
+      setKilometerErro("O valor máximo é 1000.");
+    } else {
+      setKilometerErro("");
+    }
+  };
 
-    const handlerAverageChange =(event) => {
-      const value = event.target.value;
-      if (parseInt(value, 10) > 50) {
-        setAverageError("O valor máximo é 50.");
-      } else {
-        setAverageError("");
-      }
-    };
+  const handlerAverageChange = (event) => {
+    const value = event.target.value;
+    if (parseInt(value, 10) > 50) {
+      setAverageError("O valor máximo é 50.");
+    } else {
+      setAverageError("");
+    }
+  };
 
   const renderCalculator = () => {
     return (
       <div>
-        <label htmlFor="kilometer">Informe a distância que seu veículo vai percorrer (em km)</label>
-        <input type="number" id="kilometer" min={0} max={1000} required onChange={handleKilometerChange} />
-        <span style={{color: "red"}}>{kilometerError && kilometerError}</span>
-        
-        <label htmlFor="average">Informe a média de consumo do seu veículo (em km/l)</label>
-        <input type="number" id="average" min={0} max={50} required onChange={handlerAverageChange}/>
-        <span style={{color:"red"}}>{averageError && averageError}</span>
-        
-        <label htmlFor="price">Informe o preço do combustível (por litro)</label>
-        <input type="number" id="price" min={0} step={0.01} required/>
+        <div>
+          <label htmlFor="kilometer">Informe a distância que seu veículo vai percorrer (em km)</label>
+          <input type="number" id="kilometer" min={0} max={1000} required onChange={handleKilometerChange} />
+          <span style={{ color: "red" }}>{kilometerError && kilometerError}</span>
+        </div>
+
+        <div>
+          <label htmlFor="average">Informe a média de consumo do seu veículo (em km/l)</label>
+          <input type="number" id="average" min={0} max={50} required onChange={handlerAverageChange} />
+          <span style={{ color: "red" }}>{averageError && averageError}</span>
+        </div>
+
+        <div>
+          <label htmlFor="price">Informe o preço do combustível (por litro)</label>
+          <input type="number" id="price" min={0} step={0.01} required />
+        </div>
       </div>
     );
   };
@@ -61,10 +67,16 @@ function App() {
         renderCalculator()
       ) : (
         <div>
-          <label htmlFor="username">Nome de usuário:</label>
-          <input type="text" id="username" name="username" required />
-          <label htmlFor="userid">Senha de acesso</label>
-          <input type="password" id="userid" name="userid" required />
+          <div>
+            <label htmlFor="username">Nome de usuário:</label>
+            <input type="text" id="username" name="username" required />
+          </div>
+
+          <div>
+            <label htmlFor="userid">Senha de acesso</label>
+            <input type="password" id="userid" name="userid" required />
+          </div>
+
           <input type="button" value="Entrar" onClick={validateUser} />
         </div>
       )}
